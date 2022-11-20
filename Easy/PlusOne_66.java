e08df409add8a65fa9cfc5382750314ca793e6d1
Manish Anand <89120380+firem90@users.noreply.github.com>
2022-11-20 12:31:23 +0530

    Update PlusOne_66.java

--- Easy/PlusOne_66.java
+++ Easy/PlusOne_66.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
 	
-		int[] newArr = new int[digits.length+1];
-		
-		for(int i = digits.length-1; i >= 0; i--) {
-			if(digits[i] < 9) {
-				digits[i] += 1;
-                return digits;
-            } else
-                digits[i] = 0;
-        }
+	int[] newArr = new int[digits.length+1];
 
-        newArr[0] = 1;
+	for(int i = digits.length-1; i >= 0; i--) {
+		if(digits[i] < 9) {
+		    digits[i] += 1;
+		    return digits;
+		 } 
+		digits[i] = 0;
+	}
 
-        return newArr;
+	newArr[0] = 1;
+
+	return newArr;
     }
 }
